67f74219af0417e6657e5071446814112649d645
Joaquin  Galeano <JOAQUIN.GALEANO@ort.edu.ar>
2023-09-14 15:14:33 -0300

    Init proyect

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+
+
+  export let basepath = "/prode-svelte";
+
+</script>
+
+<main>
+<Router {basepath}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="/blog/:id" component={BlogPost} />
+    <Route path="/blog" component={Blog} />
+    <Route path="/about" component={About} />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
+</main>
+
+<style>
+ 
+</style>
